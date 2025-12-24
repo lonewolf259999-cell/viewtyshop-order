51566ac41ce4f01c78cbc694567968f6418c8eb1
lonewolf259999-cell <lonewolf.259999@gmail.com>
2025-12-24 13:22:59 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,13 +1,3 @@
 const SHIRT_DESIGNS = [
     { name: "เสื้อลูกไม้", price: "1190" },
 ];
-
-(function loadProducts() {
-    const select = document.getElementById('pattern');
-    SHIRT_DESIGNS.forEach(item => {
-        const option = document.createElement('option');
-        option.value = `${item.name} (${item.price}.-)`;
-        option.textContent = `${item.name} - ${item.price} บาท`;
-        select.appendChild(option);
-    });
-})();
